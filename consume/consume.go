c43df06edc8cf78c366c058e94c31b9fd98f7ff4
Traphume <traphumedev@gmail.com>
2020-05-06 15:23:34 +0700

    Added Message and Data struct

--- consume/consume.go
+++ consume/consume.go
@@ -2,3 +2,19 @@
 // A Consumer read and validate data from some data source
 // Data is then written to data source
 package consume
+
+// Message represent the data (associated with a controller) that is read from some data source
+type Message struct {
+	Token string `json:"token" validate:"uuid4"`
+	Data  []Data `json:"data" validate:"dive"`
+}
+
+// Data are raw data points included within message
+type Data struct {
+	Timestamp    string  `json:"timestamp"`
+	Temperature  float32 `json:"temperature"`
+	Humidity     float32 `json:"humidity" validate:"gte=0,lte=100"`
+	Light        float32 `json:"light" validate:"gte=0,lte=65535"`
+	SoilMoisture int     `json:"soil_moisture" validate:"gte=0,lte=1000"`
+	WaterLevel   int     `json:"water_level" validate:"gte=0"`
+}

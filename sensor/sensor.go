186469ba9c5b5bf549da8650f400914bb97bbcad
Traphume <traphumedev@gmail.com>
2020-05-09 01:37:24 +0700

    Added DataSource interface. Added RabbitMQ type and Handler method

--- sensor/sensor.go
+++ sensor/sensor.go
@@ -1,5 +1,15 @@
 package sensor
 
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/streadway/amqp"
+	"github.com/tPhume/ags-pipeline/consumer"
+	"log"
+)
+
 // Message represent the data from sensors
 type Message struct {
 	Token string `json:"token" validate:"uuid4"`
@@ -14,4 +24,40 @@ type Data struct {
 	Light        float32 `json:"light" validate:"gte=0,lte=65535"`
 	SoilMoisture int     `json:"soil_moisture" validate:"gte=0,lte=1000"`
 	WaterLevel   int     `json:"water_level" validate:"gte=0"`
+}
+
+// Interacts with out data source
+type DataSource interface {
+	Write(ctx context.Context, msg *Message) error
+}
+
+// If data is from RabbitMQ - use this
+// Implements consumer.DataSource
+type RabbitMQ struct {
+	DataSource DataSource
+}
+
+// Handles write operation given that value is amqp.Delivery
+func (r *RabbitMQ) Write(ctx context.Context) error {
+	delivery, ok := ctx.Value("msg").(amqp.Delivery)
+	if !ok {
+		return errors.New("incorrect data type")
+	}
+
+	var msg *Message
+	if err := json.Unmarshal(delivery.Body, msg); err != nil {
+		return errors.New("problem decoding data for message " + delivery.MessageId)
+	}
+
+	if err := r.DataSource.Write(ctx, msg); err != nil {
+		return consumer.ErrFatal
+	}
+	log.Println(fmt.Sprintf("written message %s to data source", delivery.MessageId))
+
+	if err := delivery.Ack(true); err != nil {
+		log.Println("cannot ack message " + delivery.MessageId)
+		return consumer.ErrFatal
+	}
+
+	return nil
 }

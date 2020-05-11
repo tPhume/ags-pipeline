8e05f134d2afe63cf7f22b870df8a5638f94e63d
Traphume <traphumedev@gmail.com>
2020-05-11 19:02:46 +0700

    Changed data type for sensor

---
+++ cmd/playground/influxdb/insert/insert.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	influxdb2 "github.com/influxdata/influxdb-client-go"
+	"github.com/spf13/viper"
+	"github.com/tPhume/ags-pipeline/sensor"
+	"log"
+	"math"
+	"strings"
+	"time"
+)
+
+func main() {
+	var err error
+
+	// Setup configuration
+	viper.SetConfigFile("influxdb.env")
+
+	err = viper.ReadInConfig()
+	failOnError("could not read influxdb.env", err)
+
+	// Load environment variables
+	influxURL := viper.GetString("INFLUX_URL")
+	influxToken := viper.GetString("INFLUX_TOKEN")
+
+	failOnEmpty(influxURL, influxToken)
+
+	// Create Influxdb client
+	client := influxdb2.NewClient(influxURL, influxToken)
+	defer client.Close()
+
+	// Create an influxdb writer
+	influxWriter := client.WriteApiBlocking("", "test")
+
+	// Mock data
+	mock := sensor.Message{
+		Token: "test-token",
+		Data: sensor.Data{
+			Temperature:  25.4,
+			Humidity:     50.5,
+			Light:        1000,
+			SoilMoisture: 300,
+			WaterLevel:   511,
+		},
+	}
+
+	// Write new data point to influxdb
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	err = influxWriter.WritePoint(ctx, influxdb2.NewPoint(
+		"sensor",
+		map[string]string{"user_id": "test-user-id", "controller-id": "test-controller-id"},
+		map[string]interface{}{
+			"temperature":   roundFloat(mock.Data.Temperature),
+			"humidity":      roundFloat(mock.Data.Humidity),
+			"light":         roundFloat(mock.Data.Light),
+			"soil_moisture": mock.Data.SoilMoisture,
+			"water_level":   mock.Data.WaterLevel,
+		},
+		time.Now(),
+	))
+	failOnError("could not write point", err)
+}
+
+func roundFloat(x float64) float64 {
+	return math.Round(x*100) / 100
+}
+
+func failOnEmpty(env ...string) {
+	for i, e := range env {
+		if strings.TrimSpace(e) == "" {
+			log.Fatal("missing env at position ", i)
+		}
+	}
+}
+
+func failOnError(msg string, err error) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
--- sensor/sensor.go
+++ sensor/sensor.go
@@ -12,15 +12,15 @@ import (
 // Message represent the data from sensors
 type Message struct {
 	Token string `json:"token" validate:"uuid4"`
-	Data  []Data `json:"data" validate:"dive"`
+	Data  Data   `json:"data" validate:"required"`
 }
 
 // Data are raw data points included within message
 type Data struct {
 	Timestamp    string  `json:"timestamp"`
-	Temperature  float32 `json:"temperature"`
-	Humidity     float32 `json:"humidity" validate:"gte=0,lte=100"`
-	Light        float32 `json:"light" validate:"gte=0,lte=65535"`
+	Temperature  float64 `json:"temperature"`
+	Humidity     float64 `json:"humidity" validate:"gte=0,lte=100"`
+	Light        float64 `json:"light" validate:"gte=0,lte=65535"`
 	SoilMoisture int     `json:"soil_moisture" validate:"gte=0,lte=1000"`
 	WaterLevel   int     `json:"water_level" validate:"gte=0"`
 }

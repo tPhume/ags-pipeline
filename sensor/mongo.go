71b3265e6a7a5153db656936f529865865bbcb92
Traphume <traphumedev@gmail.com>
2020-05-12 15:51:43 +0700

    Implemented Mongo MetaStorage

--- sensor/mongo.go
+++ sensor/mongo.go
@@ -2,6 +2,7 @@ package sensor
 
 import (
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -10,5 +11,20 @@ type Mongodb struct {
 }
 
 func (m *Mongodb) Get(ctx context.Context, token string, meta *Meta) error {
-	panic("implement me")
+	// Query for document
+	res := m.Col.FindOne(ctx, bson.M{"token": token})
+	if res.Err() != nil {
+		if res.Err() == mongo.ErrNoDocuments {
+			return ErrBadToken
+		}
+
+		return res.Err()
+	}
+
+	// Decode to meta struct
+	if err := res.Decode(meta); err != nil {
+		return err
+	}
+
+	return nil
 }

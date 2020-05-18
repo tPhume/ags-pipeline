d978ded7d83555fa571a59fda21f55be52b86734
Traphume <traphumedev@gmail.com>
2020-05-18 23:35:05 +0700

    Modified Summary data type

--- summary/summary.go
+++ summary/summary.go
@@ -1,13 +1,10 @@
 package summary
 
+type Data map[string]float64
+
 type Summary struct {
 	Id           string
 	UserId       string
 	ControllerId string
-	Date         string
-	Temperature  float64
-	Humidity     float64
-	Light        float64
-	SoilMoisture int
-	WaterLevel   int
+	Data         Data
 }

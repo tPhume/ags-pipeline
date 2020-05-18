16afec82218dd782cb7986e94b9da7e0ad19c5bc
Traphume <traphumedev@gmail.com>
2020-05-19 00:33:45 +0700

    Added types to summary package

--- summary/summary.go
+++ summary/summary.go
@@ -1,5 +1,10 @@
 package summary
 
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+)
+
 type Data map[string]float64
 
 type Summary struct {
@@ -8,4 +13,21 @@ type Summary struct {
 	ControllerId string
 	Date         string
 	Data         Data
+}
+
+type ReadStorage interface {
+	Read(ctx context.Context, summary map[string]*Summary) error
+}
+
+type WriteStorage interface {
+	Write(ctx context.Context, summary map[string]*Summary) error
+}
+
+type Storage struct {
+	Reader ReadStorage
+	Writer WriteStorage
+}
+
+func (s *Storage) Handle(ctx *gin.Context) {
+	panic("implement me")
 }
